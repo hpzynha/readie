3567284fa80b969bf86d0467dbfc04692ad6ff92
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-04-04 12:21:20 +1030

    registerpage

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -2,7 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg_image/flutter_svg_image.dart';
 import 'package:readie/style.dart';
-import 'package:readie/widgets/bottom_appbar_controller.dart';
 import 'package:readie/widgets/buttons.dart';
 import 'package:readie/widgets/text_form_field.dart';
 import 'package:readie/widgets/text_widgets.dart';
@@ -32,48 +31,33 @@ class RegisterPage extends StatelessWidget {
             child: Column(
               children: [
                 textTitle(
-                  title: 'login.login'.tr(),
-                  text: 'login.loginToUseTheApp'.tr(),
+                  title: 'register.register'.tr(),
+                  text: 'register.personalInformation'.tr(),
                 ),
                 const SizedBox(height: 29),
                 LoginTextFormField(
-                    title: 'login.email'.tr(),
-                    hintText: 'login.enterEmail'.tr()),
+                    title: 'register.name'.tr(),
+                    hintText: 'register.enterName'.tr()),
                 const SizedBox(height: 12),
                 LoginTextFormField(
-                  title: 'login.password'.tr(),
-                  hintText: 'login.enterPassword'.tr(),
+                    title: 'register.email'.tr(),
+                    hintText: 'register.enterEmail'.tr()),
+                const SizedBox(height: 12),
+                LoginTextFormField(
+                  title: 'register.password'.tr(),
+                  hintText: 'register.enterPassword'.tr(),
+                ),
+                const SizedBox(height: 12),
+                LoginTextFormField(
+                  title: 'register.comfirmPassword'.tr(),
+                  hintText: 'register.enterComfirmPassword'.tr(),
                 ),
                 const SizedBox(height: 32),
                 PrimaryButton(
-                  title: 'login.login'.tr(),
+                  title: 'register.register'.tr(),
                   onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (ctx) => const BottomAppbarController()));
+                    Navigator.pushNamed(context, '/home');
                   },
-                ),
-                const SizedBox(height: 30),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        color: rSecondaryTextColor,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text('login.orLoginWith'.tr(),
-                          style: TextStyle(color: rSecondaryTextColor)),
-                    ),
-                    Expanded(
-                        child: Divider(
-                      color: rSecondaryTextColor,
-                      height: 24,
-                    ))
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
                 ),
               ],
             ),
--- lib/routes.dart
+++ lib/routes.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:readie/pages/auth/login_page.dart';
 import 'package:readie/pages/auth/register_page.dart';
 import 'package:readie/pages/splash_page.dart';
+import 'package:readie/widgets/bottom_appbar_controller.dart';
 
 class Routes {
   static Map<String, Widget Function(BuildContext)> list =
       <String, WidgetBuilder>{
     '/': (context) => const SplashPage(),
     '/login': (context) => const LoginPage(),
-    '/register': (context) => const RegisterPage()
+    '/register': (context) => const RegisterPage(),
+    '/home': (context) => const BottomAppbarController(),
   };
   static String initial = '/';
 
