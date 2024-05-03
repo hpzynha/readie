7867e5d94427afdd57c0f3cf7b2ea668b687031d
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-05-03 19:57:14 +0930

    grid

--- lib/pages/discover_page.dart
+++ lib/pages/discover_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:readie/style.dart';
+import 'package:readie/widgets/custom_grid.dart';
 import 'package:readie/widgets/custom_search_bar.dart';
 
-class DiscoverPage extends StatelessWidget {
+class DiscoverPage extends StatefulWidget {
   const DiscoverPage({super.key});
 
+  @override
+  State<DiscoverPage> createState() => _DiscoverPageState();
+}
+
+class _DiscoverPageState extends State<DiscoverPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +33,35 @@ class DiscoverPage extends StatelessWidget {
           )
         ],
       ),
-      body: const Text('DiscoverPage'),
+      body: Column(
+        children: [
+          const Row(
+            children: [
+              Text(
+                'Popular Genres',
+                style: TextStyle(fontSize: 24, color: Colors.black),
+              ),
+            ],
+          ),
+          const Expanded(child: CustomGrid()),
+          GestureDetector(
+            onTap: () {},
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Explore all genres',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                Icon(Icons.arrow_forward)
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/custom_grid.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:readie/style.dart';
+
+class CustomGrid extends StatelessWidget {
+  const CustomGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(
+          6,
+          (index) {
+            return Container(
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: rSecondaryColor,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
