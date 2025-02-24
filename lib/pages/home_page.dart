2d0d67d05678f096b9fa4616ef6940c8f19dcb7e
hpzynha <larissa.nogueira.rocha@gmail.com>
2025-02-24 17:47:23 -0400

    nameuser

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:readie/styles/colors.dart';
+import 'package:readie/widgets/user/user_name.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../widgets/text_widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    final userNameText = user?.displayName ?? 'Usuário';
+
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          const Expanded(
-            child: Row(
-              children: [SizedBox(width: 20), Text('oii')],
+        backgroundColor: rBackgroundColor,
+        title: textLogo(),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                userName(name: userNameText),
+                Icon(
+                  Icons.percent,
+                ),
+              ],
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/user/user_name.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../styles/colors.dart';
+
+Widget userName({required String name}) {
+  return Row(
+    children: [
+      Text(
+        'Bem Vindo, ',
+        style: GoogleFonts.lato(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+          color: rTertiart,
+        ),
+      ),
+      Text(
+        name,
+        style: GoogleFonts.lato(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+          color: rTertiart,
+        ),
+      ),
+    ],
+  );
+}
